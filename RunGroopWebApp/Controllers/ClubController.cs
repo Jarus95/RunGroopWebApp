8fb2642f83944aaebfbb9c163debfffe12ffaee6
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-06 16:27:34 +0500

    Add view

---
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            List<Club> clubs = await _context.Club.ToListAsync();
+            return View(clubs);
+        }
+    }
+}
