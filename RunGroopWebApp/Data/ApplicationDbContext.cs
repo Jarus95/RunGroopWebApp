44579ce4c30d9e26ef5a53a60f5f64d9693f5214
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-06 14:28:52 +0500

    Add-EntityFramework

---
+++ RunGroopWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<AppUser> AppUser { get; set; }
+        public DbSet<Club> Club { get; set; }
+        public DbSet<Race> Race { get; set; }
+    }
+}
