c95e11f92ae8942a7e65f0023f1f16cb75462211
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-07 11:18:35 +0500

    Add Race

--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
 
 namespace RunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var races = await _context.Race.ToListAsync();
+            return View(races);
         }
     }
 }
